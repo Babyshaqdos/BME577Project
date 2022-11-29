1f9b6c7a2cab734f52289652c5b10e0c4b141e29
Nick Souligne <44684548+Babyshaqdos@users.noreply.github.com>
2022-11-28 18:46:04 -0700

    New Main
    Redesigned a few things for clarity. Removed database implementation and started HADES Cyclops implementation for linear regression

---
+++ app.R
@@ -0,0 +1,211 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(ggpubr)
+library(dplyr)
+library(gsubfn)
+library(RSQLite)
+library(remotes)
+library("CohortGenerator")
+library("Cyclops")
+#install_github("ohdsi/Hades", upgrade = "always")
+
+serverFrame <- data.frame()
+
+# Define UI for application that performs variable analysis of CSV data sets
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Visual and Qualitative analysis of CSV Datasets"),
+    
+    #Layout defines the top portion of the UI
+    sidebarLayout(
+      
+      #This panel defines top left portion of UI, allows for user input of dataset and analysis option
+      sidebarPanel(
+        tags$h3('Please enter the dataset information and help define a cohort'),
+        inputDataText <- textInput("inputDataText", label = "Path to dataset", 
+                                   value ="",
+                                   width = "50%",
+                                   placeholder = "C:/Users/...."),
+        actionButton("Add_dataset", "Save dataset in program", size="sm", color="warning"),
+        inputDataText <- textInput("inputDataVar", label = "Cohort Criteria", value = "",
+                                   width = "50%", placeholder = "Traumatic Brain Injury, heart failure, stroke etc."),
+       verbatimTextOutput(outputId = "datasets"),
+       tags$h3('Please enter a variable and indicate dependent or independent'),
+       textInput('variable1', 'Variable 1'),
+       radioButtons("varType1", 'Variable Type', c("Dependent", "Independent")),
+       textInput('variable2', 'Variable 2'),
+       radioButtons('varType2', 'Variable Type', c("Dependent", "Independent")),
+       selectInput("selectAnalysis", "Analysis to perform", c("ANOVA", "T-Test", "Linear Regression",
+                                                              "Poisson Linear Regression", "Cox Linear Regression")),
+       actionButton('run', 'Run'),
+      ),
+      
+      #This panel contains the plots, table, and qualitative analysis summary
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Plot", plotOutput("plot")),
+          tabPanel("Summary", verbatimTextOutput(outputId = "summary")),
+          tabPanel("Table", tableOutput("table"))
+        )
+      )
+    ),
+    
+  #This panel mostly for testing purposes atm  
+  mainPanel(
+      hr(),
+      verbatimTextOutput(outputId = "text"), #contains the output in text
+  )
+    
+    
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  l <- reactiveValues()
+  l$dataplots <- list()
+  
+  
+  #Observer to watch the save dataset button, adds the filepath to a list then reads the CSV and puts it into a dataframe
+  observeEvent(input$Add_dataset, { 
+    removeModal()
+    l$filePath <- input$inputDataText
+    l$dataplots[l$filePath] <- l$filePath
+  #  df <- read.csv(l$filePath)   ##Need to include a check here to ensure the file exists, if not throws an error instead of crashing
+  #  df2 <- data.frame(df)
+  #  serverFrame <<- bind_rows(df2, serverFrame)
+
+    output$datasets <- renderPrint({ #Data check
+      if (is.null(l$dataplots)) return (NULL)
+      paste(l$dataplots)
+    })
+  })
+  
+  
+  #Puts an observer pattern on the run button, performs most functional requirements 
+  observeEvent(input$run, {
+    removeModal()
+
+    #takes the user inputted variable in the pop out menu and 
+    #assigns them to a variable in l that can be accessed by UI
+    l$variable1 <- input$variable1
+    l$variable2 <- input$variable2
+    variable1 <- l$variable1
+    variable2 <- l$variable2
+    l$varType1 <- input$varType1
+    l$varType2 <- input$varType2
+    l$plot <- input$plot
+    l$analysisType <- input$selectAnalysis
+  #  var1DF <- serverFrame[ , l$variable1]
+  #  var2DF <- serverFrame[ , l$variable2]
+
+
+    
+    #Selection of specific analysis algorithm
+    if (l$analysisType == "ANOVA"){
+      if (l$varType1 == 'Independent'){
+        
+      }
+      else if (l$varType2 == 'Independent'){
+        
+      }
+    }
+    else if (l$analysisType == "T-Test"){
+      if (l$varType1 == 'Independent'){
+        
+      }
+      else if (l$varType2 == 'Independent'){
+        
+      }
+    }
+    else if (l$analysisType == "Linear Regression"){
+      if (l$varType1 == 'Independent'){
+        count <- 0
+        for (csv in l$dataplots){
+          df <- read.csv(csv)   ##Need to include a check here to ensure the file exists, if not throws an error instead of crashing
+          df2 <- data.frame(df)
+          if (count == 0){
+            serverFrame <<- df2
+            count <- 1
+          }
+          else{
+            serverFrame <<- merge(serverFrame, df2, by = variable1)
+          }
+        }
+        cyclopsData <- createCyclopsData(serverFrame[[variable2]] ~ serverFrame[[variable1]], modelType = "lr", data = serverFrame)
+        print(summary(cyclopsData))
+        cyclopsFit <- fitCyclopsModel(cyclopsData)
+        coefficient <- coef(cyclopsFit)
+        print(coefficient)
+        confidence <- confint(cyclopsFit, c(variable1,variable2))
+        print(confidence)
+        prediction <- predict(cyclopsFit)
+        print(prediction)
+        output$summary <- renderPrint({
+          if (is.null(cyclopsData)) return (NULL)
+          paste(summary(cyclopsData))
+        })
+      }
+      else if (l$varType2 == 'Independent'){
+        
+      }
+    }
+    else if (l$analysisType == "Poisson Linear Regression"){
+      if (l$varType1 == 'Independent'){
+        
+      }
+      else if (l$varType2 == 'Independent'){
+        
+      }
+    }
+    else if (l$analysisType == "Cox Linear Regression"){
+      if (l$varType1 == 'Independent'){
+        
+      }
+      else if (l$varType2 == 'Independent'){
+        
+      }
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+     output$plot <- renderPlot({
+
+  
+    })
+    
+    output$summary <- renderPrint({
+      if (is.null(l$summary)) return (NULL)
+      paste(l$summary)
+    })
+  })
+  
+  #Mostly used for debugging purposes atm, returns user input to UI to be displayed
+  output$text <- renderPrint({
+    if (is.null(l$variable1)) return (NULL)
+    if (is.null(l$variable2)) return (NULL)
+    paste('Variable 1:', l$variable1, l$varType1, 'Variable 2:', l$variable2, l$varType2)
+  
+  })
+  
+ 
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
